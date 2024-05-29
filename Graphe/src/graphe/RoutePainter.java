f7cdd1babf0bd4bd2df6459fbae5bdeed4f9c56e
Aspect-PC <Aspect-PC@DRA3FCS>
2024-05-29 21:20:10 +0200

    New class RoutePainter

---
+++ Graphe/src/graphe/RoutePainter.java
@@ -0,0 +1,44 @@
+package graphe;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.geom.Point2D;
+import java.util.List;
+import org.jxmapviewer.JXMapViewer;
+import org.jxmapviewer.painter.Painter;
+import org.jxmapviewer.viewer.GeoPosition;
+
+public class RoutePainter implements Painter<JXMapViewer> {
+    private List<GeoPosition> track;
+
+    public RoutePainter(List<GeoPosition> track) {
+        this.track = track;
+    }
+
+    @Override
+    public void paint(Graphics2D g, JXMapViewer map, int w, int h) {
+        g = (Graphics2D) g.create();
+        Rectangle rect = map.getViewportBounds();
+        g.translate(-rect.x, -rect.y);
+
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); // Rendu des lignes quality ++
+        g.setColor(Color.BLUE); 
+        g.setStroke(new BasicStroke(1));
+
+        int lastX = -1;
+        int lastY = -1;
+
+        for (GeoPosition gp : track) {
+            Point2D pt = map.getTileFactory().geoToPixel(gp, map.getZoom());
+            if (lastX != -1 && lastY != -1) {
+                g.drawLine(lastX, lastY, (int) pt.getX(), (int) pt.getY());
+            }
+            lastX = (int) pt.getX();
+            lastY = (int) pt.getY();
+        }
+        g.dispose();
+    }
+}
