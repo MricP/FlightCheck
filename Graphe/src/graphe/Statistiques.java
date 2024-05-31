7807e15f00383b58c75997c7be4574e240a86e9b
PETIT ANGUS p2301811 <angus.petit@etu.univ-lyon1.fr>
2024-05-31 17:07:06 +0000

    Add Class Statistiques

---
+++ Graphe/src/graphe/Statistiques.java
@@ -0,0 +1,37 @@
+package graphe;
+
+public class Statistiques {
+    private double degreMoyen;
+    private int nbComposantes;
+    private int nbNoeuds;
+    private int nbAretes;
+    private double diametre;
+
+    public Statistiques(double degreMoyen, int nbComposantes, int nbNoeuds, int nbAretes, double diametre) {
+        this.degreMoyen = degreMoyen;
+        this.nbComposantes = nbComposantes;
+        this.nbNoeuds = nbNoeuds;
+        this.nbAretes = nbAretes;
+        this.diametre = diametre;
+    }
+
+    public double getDegreMoyen() {
+        return degreMoyen;
+    }
+
+    public int getNbComposantes() {
+        return nbComposantes;
+    }
+
+    public int getNbNoeuds() {
+        return nbNoeuds;
+    }
+
+    public int getNbAretes() {
+        return nbAretes;
+    }
+
+    public double getDiametre() {
+        return diametre;
+    }
+}
